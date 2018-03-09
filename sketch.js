cc04ba123691fb9fbcfabb5a9f0566e442998f80
andoniostavris <35973082+andoniostavris@users.noreply.github.com>
2018-03-09 15:55:15 -0600

    project 2

---
+++ sketch.js
@@ -0,0 +1,14 @@
+const CANVAS_WIDTH = 500;
+
+const CANVAS_HEIGHT =400;
+function setup() {
+  createCanvas( 500, 600 );
+   background( '#e3f9c8' );
+}
+function draw() {
+  fill(10,180,180)
+ellipse( 340, 70, 110, 150, 90 )
+ellipse(230,200,40,75);
+  }
+  fill(15)
+triangle( 20, 4, 150, 50, 150, 180 )
